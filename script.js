e4d10c57ce9376818353e74a2d0a329b35a37f5a
Leonardo <leeo.pereira@gmail.com>
2026-01-22 16:52:35 -0300

    dados.js tests done

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-const display = document.getElementById('produtos_lista');
-const nomeDoPrimeiro = produtos[0].nome_produto;
-display.innerText = "O primeiro produto é: "
+const display = document.getElementById('teste-dados');
+
+// Limpamos a div antes de começar
+display.innerHTML = "<h2>Lista de Produtos Carregada:</h2>";
+
+// Percorremos a lista de produtos
+produtos.forEach(item => {
+    // O += serve para ADICIONAR e não substituir o que já estava lá
+    display.innerHTML += "<p>ID: " + item.id_produto + " | Nome: " + item.nome_produto + "</p>";
+});
